67ef1cac9e55ba8e32e36e572f92e576eccdcf1d
SN-Dan <152906479+SN-Dan@users.noreply.github.com>
2025-07-10 16:16:15 +0300

    init commit 16

--- Odoo 17 plus/payment_neatworldpayvt/const.py
+++
@@ -1,6 +0,0 @@
-# Original Author: Daniel Stoynev
-# Copyright (c) 2025 SNS Software Ltd. All rights reserved.
-
-DEFAULT_PAYMENT_METHODS_CODES = [
-    'card'
-]
--- Odoo 17 plus/payment_neatworldpayvt/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-# Original Author: Daniel Stoynev
-# Copyright (c) 2025 SNS Software Ltd. All rights reserved.
-
-from . import main
