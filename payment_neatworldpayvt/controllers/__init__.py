4edf2a60424d2ac8cea7626cda01100de14ea690
SN-Dan <152906479+SN-Dan@users.noreply.github.com>
2025-07-21 08:01:18 +0300

    Corrected licensing

--- payment_neatworldpayvt/controllers/__init__.py
+++ payment_neatworldpayvt/controllers/__init__.py
@@ -1,4 +1,6 @@
 # Original Author: Daniel Stoynev
 # Copyright (c) 2025 SNS Software Ltd. All rights reserved.
+# This module extends Odoo's payment framework.
+# Odoo is a trademark of Odoo S.A.
 
 from . import main
